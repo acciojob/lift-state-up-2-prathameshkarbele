3ee425daf21c575d35c8c9b72de99240e5110db0
Prathamesh karbele <113411026+prathameshkarbele@users.noreply.github.com>
2023-03-30 22:06:49 +0530

    Update App.js
    p1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import '../styles/App.css';
+import ChildComponent from "./ChildComponent";
 
-const App = () => {
+function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+    console.log(event.target.value);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div class="parent">
+      <h1>Parent Component</h1>
+      {inputValue}
+      <ChildComponent
+        inputValue={inputValue}
+        handleInputChange={handleInputChange}
+      />
     </div>
-  )
+  );
 }
 
 export default App
